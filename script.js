b1651fadb31da5c5062fb32d40941775830a995b
Javeria Kamran <javeriakamran339@gmail.com>
2024-09-18 23:21:49 +0500

    Loops over an Array

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log (hello world!)
+//Creater Arrays
+let marks=[96,98,97,95,94]
+console.log(marks);
+console.log(marks.length);
+
+let heroes=["ironman","superman","spiderman"] 
+for (let i=0; i<heroes.length;i++){
+    console.log(heroes[i]);
+}
+let cities=["delhi","pune","mumbai"]
+for(let city of cities){
+    console.log(city);
+}
