17f3d4e221acdfef54428a675e9b50b2e660c7a0
Javeria Kamran <javeriakamran339@gmail.com>
2024-09-20 20:22:50 +0500

    Functions

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
-//Creater Arrays
-let marks=[96,98,97,95,94]
-console.log(marks);
-console.log(marks.length);
-
-let heroes=["ironman","superman","spiderman"] 
-for (let i=0; i<heroes.length;i++){
-    console.log(heroes[i]);
+function myFunction() {
+    console.log("welcome to my page");
 }
-let cities=["delhi","pune","mumbai"]
-for(let city of cities){
-    console.log(city);
-}
-let mark =[85,97,44,37,76,60];
-let sum=0;
-for (let val of mark) {
-    sum += val;
-}
-let avg= sum/marks.length; 
-console.log(`avg marks o the class= ${avg}`);
+myFunction();
